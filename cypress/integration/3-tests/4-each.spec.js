2c32b2bb47c9d7466d1cbdf5da9980c3d2b47169
Andy <andyarias17@gmail.com>
2022-06-01 20:45:41 -0600

    Variables de ambientes y debug

--- cypress/integration/3-tests/4-each.spec.js
+++ cypress/integration/3-tests/4-each.spec.js
@@ -13,13 +13,17 @@ describe('Each Suite', () => {
         cy.visit('https://www.booking.com/')
         cy.get('.bui-header__tab li a').find('span.bui-tab__text').each((myMenu, index, list) => {
             console.log(myMenu.text())
+            console.log(index)
+            console.log(list)
+            
             //cy.wrap(myMenu).should('have.text', menuList[index])
             cy.wrap(myMenu).invoke('text').then((textoMenu) => {
                 expect(textoMenu.replaceAll('\n', '')).to.contain(menuList[index])
             })
-            
-            
-            
+
+            if(myMenu.text().includes('Attractions')){
+                cy.wrap(list).eq(4).click()
+            }
         })       
         
     })
---
+++ cypress/integration/3-tests/8-Var Ambiente.js
@@ -0,0 +1,31 @@
+describe('Alias Element', () => {
+    it('then promise', () => {
+        //EJEMPLO DE TERMINAL 
+        // npx cypress open --env username=andy,password=mypass
+        
+        console.log(Cypress.env('myVar')) //obtenemos
+        Cypress.env('HOTEL', 'Cabañas Don Camilo Albergue de Montaña') //asignamos
+        let value = "Calathea Lodge Monteverde Monteverde, Costa Rica"
+
+        cy.visit('https://www.booking.com/')
+        cy.get('#ss').type(value)
+        cy.get('[aria-controls="xp__guests__inputs-container"]').parent().click()
+        cy.get('#xp__guests__inputs-container').should('be.visible')
+        cy.get('.sb-group__field-adults button').eq(1).click()
+        cy.get('.xp__button [data-sb-id="main"]').click()
+        cy.get('[data-testid="property-card"]').as('propertyCard')
+        cy.get('@propertyCard').eq(0).find('[data-testid="title"]').invoke('text').then((myText) => {
+            if(myText.includes(Cypress.env('HOTEL'))){
+                console.log(Cypress.env('username'))
+                console.log(Cypress.env('password'))
+                console.log('TRUE')
+            }else{
+                console.log('FALSE')
+            }
+            //convert dates js
+
+        })
+        cy.get('@propertyCard').eq(0).find('[data-testid="review-score"]').find('div').eq(0).should('gt', 8)
+        console.log('Hola1')
+    })
+})
