1c19f8892279b498be833a15d08c3c2969d7d575
Andy <andyarias17@gmail.com>
2022-06-01 19:57:54 -0600

    Ejemplo Each

---
+++ cypress/integration/3-tests/4-each.spec.js
@@ -0,0 +1,26 @@
+describe('Each Suite', () => {
+    it('Each Test', () => {
+        const menuList = [
+            "Stays",
+            "Flights",
+            "Flight + Hotel",
+            "Car rentals",
+            "Attractions",
+            "Airport taxis"
+        ]
+
+
+        cy.visit('https://www.booking.com/')
+        cy.get('.bui-header__tab li a').find('span.bui-tab__text').each((myMenu, index, list) => {
+            console.log(myMenu.text())
+            //cy.wrap(myMenu).should('have.text', menuList[index])
+            cy.wrap(myMenu).invoke('text').then((textoMenu) => {
+                expect(textoMenu.replaceAll('\n', '')).to.contain(menuList[index])
+            })
+            
+            
+            
+        })       
+        
+    })
+})
